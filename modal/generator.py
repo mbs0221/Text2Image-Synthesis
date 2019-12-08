4b720936c33829a44ecdb9c176ea32ecc7bd8bd9
mbs0221 <mbs2021@163.com>
2019-12-08 17:27:38 +0800

    run-with-gpu

--- modal/generator.py
+++ modal/generator.py
@@ -21,19 +21,19 @@ class MBSGenerator(nn.Module):
             # [-, nz, 1, 1]
             nn.ConvTranspose2d(latent_dim + text_dim, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True),
+            nn.LeakyReLU(0.2, True),
             # [-, 1024, 4, 4]
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
-            nn.ReLU(True),
+            nn.LeakyReLU(0.2, True),
             # [-, 512, 8, 8]
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
-            nn.ReLU(True),
+            nn.LeakyReLU(0.2, True),
             # [-, 256, 16, 16]
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
-            nn.ReLU(True),
+            nn.LeakyReLU(0.2, True),
             # [-, 128, 32, 32]
             nn.ConvTranspose2d(ngf, n_channels, 4, 2, 1, bias=False),
             nn.BatchNorm2d(n_channels),
